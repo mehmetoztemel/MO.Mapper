2d9bf0d1732c7f547f071d97c88aaef0d4f7e18f
Mehmet Öztemel <mehmet-oztemel@hotmail.com>
2026-05-15 00:12:59 +0300

    Optimize Mapper performance and add nullability
    Refactor Mapper to reduce allocations and improve performance: use arrays (ToArray) instead of lists/dictionaries, replace LINQ FirstOrDefault/OrderBy with direct loops, and preallocate result lists. Add nullable annotations for optional target parameters and change collection Map to throw on null source. Also improve constructor selection logic, use object?[] for constructor args, and add type-assignability checks when mapping constructor parameters and properties.

--- Mapper.cs
+++ Mapper.cs
@@ -4,24 +4,45 @@ namespace MO.Mapper
 {
     public static class Mapper
     {
-        public static TTarget Map<TSource, TTarget>(TSource source, TTarget target = null) where TTarget : class
+        public static TTarget Map<TSource, TTarget>(TSource source, TTarget? target = null) where TTarget : class
         {
             if (source == null)
             {
                 throw new ArgumentNullException(nameof(source));
             }
 
-            //List<PropertyInfo> sourceProperties = (from p in typeof(TSource).GetProperties(BindingFlags.Instance | BindingFlags.Public)
-            //                                       where p.CanRead
-            //                                       select p).ToList();
-
-            List<PropertyInfo> sourceProperties = typeof(TSource).GetProperties(BindingFlags.Instance | BindingFlags.Public)
-                                                                 .Where(p => p.CanRead)
-                                                                 .ToList();
+            // ToList() yerine array kullanın - daha az allocation
+            PropertyInfo[] sourceProperties = typeof(TSource)
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .Where(p => p.CanRead)
+                .ToArray();
 
             if (target == null)
             {
-                ConstructorInfo constructorInfo = typeof(TTarget).GetConstructors(BindingFlags.Instance | BindingFlags.Public).FirstOrDefault();
+                var constructors = typeof(TTarget).GetConstructors(BindingFlags.Instance | BindingFlags.Public);
+
+                // OrderBy yerine tek döngüde parametre sayısına göre bulun
+                ConstructorInfo? constructorInfo = null;
+                ConstructorInfo? minParamConstructor = null;
+                int minParamCount = int.MaxValue;
+
+                foreach (var ctor in constructors)
+                {
+                    int paramCount = ctor.GetParameters().Length;
+                    if (paramCount == 0)
+                    {
+                        constructorInfo = ctor;
+                        break;
+                    }
+                    if (paramCount < minParamCount)
+                    {
+                        minParamCount = paramCount;
+                        minParamConstructor = ctor;
+                    }
+                }
+
+                constructorInfo ??= minParamConstructor;
+
                 if (constructorInfo == null)
                 {
                     throw new InvalidOperationException("No public constructor found.");
@@ -34,59 +55,79 @@ namespace MO.Mapper
                 }
                 else
                 {
-                    object[] constructorParams = new object[parameters.Length];
+                    object?[] constructorParams = new object?[parameters.Length];
                     for (int i = 0; i < parameters.Length; i++)
                     {
                         ParameterInfo param = parameters[i];
-                        PropertyInfo propertyInfo = sourceProperties.FirstOrDefault((PropertyInfo p) => p.Name.Equals(param.Name, StringComparison.OrdinalIgnoreCase));
-                        if (propertyInfo != null)
+
+                        // LINQ FirstOrDefault yerine for loop
+                        PropertyInfo? propertyInfo = null;
+                        for (int j = 0; j < sourceProperties.Length; j++)
+                        {
+                            if (sourceProperties[j].Name.Equals(param.Name, StringComparison.OrdinalIgnoreCase))
+                            {
+                                propertyInfo = sourceProperties[j];
+                                break;
+                            }
+                        }
+
+                        if (propertyInfo != null && param.ParameterType.IsAssignableFrom(propertyInfo.PropertyType))
                         {
                             constructorParams[i] = propertyInfo.GetValue(source);
                         }
                         else
                         {
-                            constructorParams[i] = (param.ParameterType.IsValueType ? Activator.CreateInstance(param.ParameterType) : null);
+                            constructorParams[i] = param.ParameterType.IsValueType
+                                ? Activator.CreateInstance(param.ParameterType)
+                                : null;
                         }
                     }
                     target = (TTarget)constructorInfo.Invoke(constructorParams);
                 }
             }
-            //Dictionary<string, PropertyInfo> targetProperties = (from p in typeof(TTarget).GetProperties(BindingFlags.Instance | BindingFlags.Public)
-            //                                                     where p.CanWrite
-            //                                                     select p).ToDictionary((PropertyInfo p) => p.Name, (PropertyInfo p) => p);
 
-            Dictionary<string, PropertyInfo> targetProperties = typeof(TTarget)
+            // Target properties alın
+            PropertyInfo[] targetProperties = typeof(TTarget)
                 .GetProperties(BindingFlags.Instance | BindingFlags.Public)
                 .Where(p => p.CanWrite)
-                .ToDictionary(p => p.Name, p => p);
+                .ToArray();
 
+            // Nested loop - küçük property sayıları için Dictionary'den daha hızlı
             foreach (PropertyInfo sourceProp in sourceProperties)
             {
-                if (targetProperties.TryGetValue(sourceProp.Name, out var targetProp) && targetProp.PropertyType.IsAssignableFrom(sourceProp.PropertyType))
+                for (int i = 0; i < targetProperties.Length; i++)
                 {
-                    targetProp.SetValue(target, sourceProp.GetValue(source));
+                    PropertyInfo targetProp = targetProperties[i];
+                    if (targetProp.Name == sourceProp.Name &&
+                        targetProp.PropertyType.IsAssignableFrom(sourceProp.PropertyType))
+                    {
+                        targetProp.SetValue(target, sourceProp.GetValue(source));
+                        break;
+                    }
                 }
             }
 
             return target;
         }
 
-        public static List<TTarget> Map<TSource, TTarget>(IEnumerable<TSource> source, IEnumerable<TTarget> target = null) where TTarget : class
+        public static List<TTarget> Map<TSource, TTarget>(IEnumerable<TSource> source, IEnumerable<TTarget>? target = null) where TTarget : class
         {
             if (source == null)
             {
-                return new List<TTarget>();
+                throw new ArgumentNullException(nameof(source));
             }
 
-            List<TTarget> result = new List<TTarget>();
-            var targetList = target?.ToList() ?? new List<TTarget>();
-            var targetCount = targetList.Count;
+            var sourceList = source as List<TSource> ?? source.ToList();
+            var targetList = target as List<TTarget> ?? target?.ToList() ?? new List<TTarget>();
 
-            for (int i = 0; i < source.Count(); i++)
+            // Capacity belirterek allocation'ı azaltın
+            List<TTarget> result = new List<TTarget>(sourceList.Count);
+            int targetCount = targetList.Count;
+
+            for (int i = 0; i < sourceList.Count; i++)
             {
-                TSource currentSource = source.ElementAt(i);
-                TTarget currentTarget = i < targetCount ? targetList[i] : null;
-                result.Add(Map(currentSource, currentTarget));
+                TTarget? currentTarget = i < targetCount ? targetList[i] : null;
+                result.Add(Map(sourceList[i], currentTarget));
             }
 
             return result;
